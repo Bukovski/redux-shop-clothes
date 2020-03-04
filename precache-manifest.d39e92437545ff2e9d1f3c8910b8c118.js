705044b595b5d8541f777a184d9d1052d9a0783c
Bukovski <kmanankov@bk.ru>
2020-03-04 09:45:26 +0300

    Updates

---
+++ precache-manifest.d39e92437545ff2e9d1f3c8910b8c118.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3094149aa52ba8f04dfc839e8cbb0d7a",
+    "url": "/redux-shop-clothes/index.html"
+  },
+  {
+    "revision": "d278994f15f1e7f5f2a6",
+    "url": "/redux-shop-clothes/static/css/main.1e006de9.chunk.css"
+  },
+  {
+    "revision": "fa0849695e9f079e32b2",
+    "url": "/redux-shop-clothes/static/js/2.7f10ce92.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/redux-shop-clothes/static/js/2.7f10ce92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d278994f15f1e7f5f2a6",
+    "url": "/redux-shop-clothes/static/js/main.fd381e67.chunk.js"
+  },
+  {
+    "revision": "80c9ef0b9b0a0ec4f3ad",
+    "url": "/redux-shop-clothes/static/js/runtime-main.87fcc515.js"
+  },
+  {
+    "revision": "594fb5fe5d92a32f0c13e6d836c5e883",
+    "url": "/redux-shop-clothes/static/media/crown.594fb5fe.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/redux-shop-clothes/static/media/shopping-bag.30f07071.svg"
+  }
+]);
